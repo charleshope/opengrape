9f66f0bce358786d945cdbc2c030afab172d5527
Charles HOPE <charles@hopeblockchain.io>
2022-08-26 17:15:04 -0400

    Add optional timeout to fetch
    New method signature that includes a timeout for fetching, because
    some URLs just hang forever.

--- src/main/java/io/github/kientux/OpenGrape.java
+++ src/main/java/io/github/kientux/OpenGrape.java
@@ -10,7 +10,9 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.*;
 import java.util.stream.Collectors;
 
 public class OpenGrape {
@@ -18,6 +20,10 @@ public class OpenGrape {
 
     public OpenGrape(String htmlString, OpenGrapeParser parser) {
         this.source = parser.parse(htmlString);
+    }
+
+    private OpenGrape() {
+        source = new HashMap<>();
     }
 
     public static OpenGrape fetch(String url) throws IOException, OpenGrapeResponseException {
@@ -35,6 +41,32 @@ public class OpenGrape {
                 .lines()
                 .collect(Collectors.joining("\n"));
         return new OpenGrape(htmlString, new DefaultOpenGrapeParser());
+    }
+
+    public static OpenGrape fetch(String url, int timeoutSeconds) throws IOException, OpenGrapeResponseException {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Future<OpenGrape> future = executor.submit(() -> OpenGrape.fetch(url));
+        try {
+            return future.get(timeoutSeconds, TimeUnit.SECONDS);
+        } catch (TimeoutException e) {
+            future.cancel(true);
+        } catch (ExecutionException e) {
+            if (e.getCause() != null) {
+                if (e.getCause() instanceof IOException) {
+                    throw (IOException) e.getCause();
+                }
+                if (e.getCause() instanceof OpenGrapeResponseException) {
+                    throw (OpenGrapeResponseException) e.getCause();
+                }
+            }
+            e.printStackTrace();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            executor.shutdownNow();
+        }
+        return new OpenGrape();
     }
 
     public String getValue(OpenGrapeMetadata metadata) {
--- src/test/java/io/github/kientux/OpenGrapeTest.java
+++ src/test/java/io/github/kientux/OpenGrapeTest.java
@@ -1,61 +1,73 @@
 package io.github.kientux;
+
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class OpenGrapeTest {
 
     @Test
-    public void givenUrl_whenParseOGTag_thenCorrect() {
+    public void givenUrl_whenParseOGTag_thenCorrect() throws Exception {
         String url = "https://opengraphprotocol.org/";
-        try {
-            OpenGrape og = OpenGrape.fetch(url);
-            String title = og.getValue(OpenGrapeMetadata.TITLE);
-            String description = og.getValue(OpenGrapeMetadata.DESCRIPTION);
-            String image = og.getValue(OpenGrapeMetadata.IMAGE);
-            assertEquals("Open Graph protocol", title);
-            assertEquals("https://ogp.me/logo.png", image);
-            assertEquals("The Open Graph protocol enables any web page to become a rich object in a social graph.", description);
-        } catch (IOException | OpenGrapeResponseException e) {
-            e.printStackTrace();
-            fail();
-        }
+        OpenGrape og = OpenGrape.fetch(url);
+        String title = og.getValue(OpenGrapeMetadata.TITLE);
+        String description = og.getValue(OpenGrapeMetadata.DESCRIPTION);
+        String image = og.getValue(OpenGrapeMetadata.IMAGE);
+        assertEquals("Open Graph protocol", title);
+        assertEquals("https://ogp.me/logo.png", image);
+        assertEquals("The Open Graph protocol enables any web page to become a rich object in a social graph.",
+                description);
     }
 
     @Test
-    public void givenShortenedUrl_whenParseOGTag_thenCorrect() {
+    public void givenShortenedUrl_whenParseOGTag_thenCorrect() throws Exception {
         String url = "https://bit.ly/3SGKNmU";
-        try {
-            OpenGrape og = OpenGrape.fetch(url);
-            String title = og.getValue(OpenGrapeMetadata.TITLE);
-            String description = og.getValue(OpenGrapeMetadata.DESCRIPTION);
-            String image = og.getValue(OpenGrapeMetadata.IMAGE);
-            assertEquals("Open Graph protocol", title);
-            assertEquals("https://ogp.me/logo.png", image);
-            assertEquals("The Open Graph protocol enables any web page to become a rich object in a social graph.", description);
-        } catch (IOException | OpenGrapeResponseException e) {
-            e.printStackTrace();
-            fail();
-        }
+        OpenGrape og = OpenGrape.fetch(url);
+        String title = og.getValue(OpenGrapeMetadata.TITLE);
+        String description = og.getValue(OpenGrapeMetadata.DESCRIPTION);
+        String image = og.getValue(OpenGrapeMetadata.IMAGE);
+        assertEquals("Open Graph protocol", title);
+        assertEquals("https://ogp.me/logo.png", image);
+        assertEquals("The Open Graph protocol enables any web page to become a rich object in a social graph.",
+                description);
     }
 
     @Test
-    public void givenShortenedUrlWithTrailingSlash_whenParseOGTag_thenCorrect() {
+    public void givenShortenedUrlWithTrailingSlash_whenParseOGTag_thenCorrect() throws Exception {
         String url = "https://bit.ly/3SGKNmU/";
-        try {
-            OpenGrape og = OpenGrape.fetch(url);
-            String title = og.getValue(OpenGrapeMetadata.TITLE);
-            String description = og.getValue(OpenGrapeMetadata.DESCRIPTION);
-            String image = og.getValue(OpenGrapeMetadata.IMAGE);
-            assertEquals("Open Graph protocol", title);
-            assertEquals("https://ogp.me/logo.png", image);
-            assertEquals("The Open Graph protocol enables any web page to become a rich object in a social graph.", description);
-        } catch (IOException | OpenGrapeResponseException e) {
-            e.printStackTrace();
-            fail();
-        }
+        OpenGrape og = OpenGrape.fetch(url);
+        String title = og.getValue(OpenGrapeMetadata.TITLE);
+        String description = og.getValue(OpenGrapeMetadata.DESCRIPTION);
+        String image = og.getValue(OpenGrapeMetadata.IMAGE);
+        assertEquals("Open Graph protocol", title);
+        assertEquals("https://ogp.me/logo.png", image);
+        assertEquals("The Open Graph protocol enables any web page to become a rich object in a social graph.",
+                description);
+    }
+
+    @Test
+    public void givenUrl_whenParseOGTagWithTimeout_thenCorrect() throws Exception {
+        String url = "https://opengraphprotocol.org/";
+        OpenGrape og = OpenGrape.fetch(url, 5);
+        String title = og.getValue(OpenGrapeMetadata.TITLE);
+        String description = og.getValue(OpenGrapeMetadata.DESCRIPTION);
+        String image = og.getValue(OpenGrapeMetadata.IMAGE);
+        assertEquals("Open Graph protocol", title);
+        assertEquals("https://ogp.me/logo.png", image);
+        assertEquals("The Open Graph protocol enables any web page to become a rich object in a social graph.",
+                description);
+    }
+
+    @Test
+    public void givenUrlThatNeverReturns_whenParseOGTagWithTimeout_thenReturns() throws Exception {
+        String url = "https://www.nasdaq.com/articles/whole-foods-is-sued-over-no-antibiotics-ever-beef-claim";
+        OpenGrape og = OpenGrape.fetch(url, 1);
+        String title = og.getValue(OpenGrapeMetadata.TITLE);
+        String description = og.getValue(OpenGrapeMetadata.DESCRIPTION);
+        String image = og.getValue(OpenGrapeMetadata.IMAGE);
+        assertNull(title);
+        assertNull(image);
+        assertNull(description);
     }
 }
